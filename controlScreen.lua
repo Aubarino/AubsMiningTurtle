2c1eb7e2b1d78f2cf0a26803269dac7afd4d9dbe
Aubarino <53990174+Aubarino@users.noreply.github.com>
2025-06-02 15:44:15 +1000

    bnbvncvbndfhd

--- controlScreen.lua
+++ controlScreen.lua
@@ -12,7 +12,7 @@ local input = ""
 local mon = peripheral.wrap("left")
 if mon then mon.setTextScale(0.5) end
 print("Status : ONLINE")
-print("version b")
+print("version c")
 if not mon then
     print("Monitor not found!")
     return
@@ -22,17 +22,16 @@ local function draw()
     for id, pos in pairs(turtles) do
         local relX = pos.x - origin.x
         local relZ = pos.z - origin.z
-        display.write(id .. ": X=" .. relX .. " Z=" .. relZ .. "\n")
+        mon.write(id .. ": X=" .. relX .. " Z=" .. relZ .. "\n")
     end
 end
 
 -- Listen for position updates
 while true do
     local _, message, protocol = rednet.receive("turtlePosData")
-    local display = mon or term
-    display.clear()
-    display.setCursorPos(1, 1)
-    display.write("Aub turtle HQ\nStatus : ONLINE\n")
+    mon.clear()
+    mon.setCursorPos(1, 1)
+    mon.write("Aub turtle HQ\nStatus : ONLINE\n")
     if type(message) == "table" and message.id then
         turtles[message.id] = {x = message.x, z = message.z}
         draw()
@@ -45,8 +44,7 @@ while true do
 end
 
 print("ended")
-local display = mon or term
-display.clear()
-display.setCursorPos(1, 1)
-display.write("Aub turtle HQ\nStatus : OFFLINE")
+mon.clear()
+mon.setCursorPos(1, 1)
+mon.write("Aub turtle HQ\nStatus : OFFLINE")
 print("Status : OFFLINE")
